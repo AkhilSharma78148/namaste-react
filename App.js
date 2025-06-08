0e7545d9c78c7adf041b8b42dba21f7dce37c6a0
Akhil Sharma <akhil.sharma1@bjshomedelivery.com>
2025-06-08 09:58:02 +0530

    react component, composition

--- App.js
+++ App.js
@@ -10,9 +10,36 @@ const heading4 = ( //JSX expression
     </h1>
 );
 
+//Functional component
+const Title = () => (
+    <h1 key="h1">Namaste React Main Heading</h1>
+);
+
+//
+const subTitle = ( //React Element
+    <h2 key="h2">Namaste React Subheading</h2>
+);
+
+//React Component
+    //Class Based Component
+    //Functional component
+
+// Functional Component
+const HeaderComponent = () => {
+    //Also Return some piece of react element
+    return (
+            <div>
+                <Title/> { /* Calling another component inside the component => also called as Composition/composite component  */}
+                {subTitle} {/*  React Element call inside the component */}
+                <h3>Namaste React Functionaly Component</h3>
+            </div>
+    )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 
 //passing a react element inside the root
-root.render(heading4);
+root.render(<HeaderComponent/>);
