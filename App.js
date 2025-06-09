55a71fb0772bb5c5b7e82c84a05c8404756c6868
Akhil Sharma <akhil.sharma1@bjshomedelivery.com>
2025-06-09 17:20:07 +0530

    props in react

--- App.js
+++ App.js
@@ -35,39 +35,84 @@ const Header = () => {
     )
 }
 
-const restrauntObj = {
-    name: "Burger King",
-    img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh",
-    place: "YamunaNagar",
-    causines: ["Dal", "Roti"],
-    rating: 4.3
-}
+const restrauntList = [
+    {
+        name: "Burger King",
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh",
+        place: "YamunaNagar",
+        causines: ["Dal", "Roti"],
+        rating: 4.3
+    },
+    {
+        name: "Burger Singh",
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/gp1ityra6utvzqn6ghnv",
+        place: "YamunaNagar",
+        causines: ["Dal", "Rice"],
+        rating: 4.3
+    },
+    {
+        name: "KFC",
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rvxp5xbniat84r6efku2",
+        place: "YamunaNagar",
+        causines: ["Paneer", "Roti"],
+        rating: 4.3
+    },
+    {
+        name: "Mr. King",
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh",
+        place: "YamunaNagar",
+        causines: ["Dal", "Roti"],
+        rating: 4.3
+    },{
+        name: "Gupta King",
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh",
+        place: "YamunaNagar",
+        causines: ["Dal", "Roti"],
+        rating: 4.3
+    },{
+        name: "MCD King",
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh",
+        place: "YamunaNagar",
+        causines: ["Dal", "Roti"],
+        rating: 4.3
+    },{
+        name: "Star Bucks",
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh",
+        place: "YamunaNagar",
+        causines: ["Dal", "Roti"],
+        rating: 4.3
+    },{
+        name: "Test King",
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh",
+        place: "YamunaNagar",
+        causines: ["Dal", "Roti"],
+        rating: 4.3
+    }
+];
 
-const RestrauntCard = () => {
+// const RestrauntCard = ({restaurant}) => { 
+const RestrauntCard = (props) => {
     return (
         <div className="card">
-            <img src={restrauntObj.img} alt="Restraunt Img" className="restraunt-img"/>
-            <h2>{restrauntObj.name}</h2>
-            <h3>{restrauntObj.place}</h3>
-            <h3>{restrauntObj.causines.join(", ")}</h3>
-            <h5>{restrauntObj.rating}</h5>
+            <img src={props.restaurant?.img} alt="Restraunt Img" className="restraunt-img"/>
+            <h2>{props.restaurant?.name}</h2>
+            <h3>{props.restaurant?.place}</h3>
+            <h5>{props.restaurant?.rating}</h5>
         </div>
     )
 }
 
+//props - properties
+
 const Body = () => {
     return (
         <div className="restraunt-list">
-           <RestrauntCard />
-           <RestrauntCard />
-           <RestrauntCard />
-           <RestrauntCard />
-           <RestrauntCard />
-           <RestrauntCard />
-           <RestrauntCard />
-           <RestrauntCard />
-           <RestrauntCard />
-           <RestrauntCard />
+           <RestrauntCard restaurant={restrauntList[0]}/>
+           <RestrauntCard restaurant={restrauntList[1]}/>
+           <RestrauntCard restaurant={restrauntList[2]}/>
+           <RestrauntCard restaurant={restrauntList[3]}/>
+           <RestrauntCard restaurant={restrauntList[4]}/>
+           <RestrauntCard restaurant={restrauntList[5]}/>
         </div>
     )
 }
